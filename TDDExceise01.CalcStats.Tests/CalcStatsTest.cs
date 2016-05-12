e8d473e86d999a235cd8fe91f56be87f0e7bcfbc
Sivakumar <bcm.sivakumar@gmail.com>
2016-05-12 21:52:39 +0530

    Adding Calc Stats project with Initial stage of TDD approach

---
+++ TDDExceise01.CalcStats.Tests/CalcStatsTest.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TDDExecise01CalcStats;
+
+namespace TDDExceise01CalcStats.Tests
+{
+    [TestClass]
+    public class CalcStatsTest
+    {
+        [TestMethod]
+        [ExpectedException(typeof(NullReferenceException))]
+        public void MinValue_Without_ClassInstance()
+        {
+            CalcStats calcStat = null;
+            int? minValue = calcStat.Min();
+            Assert.AreEqual(null, minValue);
+        }
+
+        [TestMethod]
+        public void MinValue_With_ClassInstance_with_ParameterlessConstructor()
+        {
+            CalcStats calcStat = new CalcStats();
+            int? minValue = calcStat.Min();
+
+            Assert.AreEqual(null, minValue);
+        }
+
+        [TestMethod]
+        public void MinValue_With_ClassInstance_with_ParameterisedConstructor_NullAsSequenceNumber()
+        {
+            CalcStats calcStat = new CalcStats(null);
+            int? minValue = calcStat.Min();
+
+            Assert.AreEqual(null, minValue);
+        }        
+
+        [TestMethod]
+        public void MinValue_With_EmptySequenceNumber()
+        {
+            CalcStats calcStat=new CalcStats(new int[] { });
+            int? minValue = calcStat.Min();
+
+            Assert.IsNull(minValue);
+        }
+
+        [TestMethod]
+        public void MinValue_With_SingleSequenceNumber()
+        {
+            CalcStats calcStat = new CalcStats(new int[] { 2 });
+            int? minValue = calcStat.Min();
+
+            Assert.AreEqual(2, minValue);
+        }
+
+        [TestMethod]
+        public void MinValue_with_TwoSequenceNumbers()
+        {
+            CalcStats calcStat = new CalcStats(new int[] { 2, 1 });
+            int? minValue = calcStat.Min();
+
+            Assert.AreEqual(1, minValue);
+        }
+
+    }
+}
---
+++ TDDExecise01.CalcStats/CalcStats.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDDExecise01CalcStats
+{
+    public class CalcStats
+    {
+        private int[] sequenceNumbers;
+
+        public CalcStats()
+        {
+        }
+
+        public CalcStats(int[] sequenceNumbers)
+        {
+            this.sequenceNumbers = sequenceNumbers;
+        }
+
+        public int? Min()
+        {
+            int? min = null;
+            if (sequenceNumbers == null)
+            {
+                min = null;
+            }
+            else if (sequenceNumbers.Length == 1)
+            {
+                min = sequenceNumbers[0];
+            }
+            else if(sequenceNumbers.Length==2)
+            {
+                if(sequenceNumbers[0]<sequenceNumbers[1])
+                {
+                    min = sequenceNumbers[0];
+                }
+                else
+                {
+                    min = sequenceNumbers[1];
+                }
+            }
+            return min;
+        }
+    }
+}
